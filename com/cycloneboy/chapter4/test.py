93c69b5d7be0261b15aa2b92a98fc3819af0f616
CycloneBoy <xuanfeng1992@gmail.com>
2017-06-20 22:47:55 +0800

    Initial commit

---
+++ com/cycloneboy/chapter4/test.py
@@ -0,0 +1,19 @@
+import urllib.request
+
+file = urllib.request.urlopen("http://www.baidu.com")
+
+data = file.read()
+dataline = file.readline()
+print(dataline)
+
+print(data)
+
+fhandle =open("D:/python/download/myweb/part4/1.html","wb")
+fhandle.write(data)
+fhandle.close()
+
+filename = urllib.request.urlretrieve("http://edu.51cto.com",
+                                      filename="D:/python/download/myweb/part4/2.html"
+                                      )
+
+urllib.request.urlcleanup()
