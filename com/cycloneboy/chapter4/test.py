68661965e7a9d38d0078d44787dec9d2e51c5020
CycloneBoy <xuanfeng1992@gmail.com>
2017-06-20 22:52:41 +0800

    完成注释

--- com/cycloneboy/chapter4/test.py
+++ com/cycloneboy/chapter4/test.py
@@ -8,12 +8,15 @@ print(dataline)
 
 print(data)
 
+#保存网页到D盘
 fhandle =open("D:/python/download/myweb/part4/1.html","wb")
 fhandle.write(data)
 fhandle.close()
 
+#直接爬取网页到D盘
 filename = urllib.request.urlretrieve("http://edu.51cto.com",
                                       filename="D:/python/download/myweb/part4/2.html"
                                       )
 
+#清除URLretrieve执行所造成的缓存
 urllib.request.urlcleanup()
